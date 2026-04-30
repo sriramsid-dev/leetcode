c14cf732056d9b7190e9d74d101a95de894b3231
sriramsid-dev <mail2sidsriram@gmail.com>
2026-04-30 23:31:08 +0530

    Time: 4 ms (55.85%), Space: 43.4 MB (72.53%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,27 +1,6 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        //Stack<Character> stack=new Stack<>();
-    //     stack.push(s.charAt(0));
-    //     for(int i=1;i<s.length();i++)
-    //     {
-    //         if(stack.peek()==s.charAt(i))
-    //         {
-    //             stack.pop();
-    //             stack.push(s.charAt(i));
-    //         }
-    //         else
-    //         {
-    //             stack.push(s.charAt(i));
-    //         }
-    //     }
-    //     String k="";
-    //     for(char c:stack)
-    //     {
-    //         k=k+c;
-    //     }
-    //     return k;
-    // }
-    String k="";
+    StringBuffer k=new StringBuffer("");
     int c=0;
     for(int i=0;i<s.length();i++)
     {
@@ -29,16 +8,16 @@ class Solution {
         {
             if(c>0)
             {
-                k=k+s.charAt(i);
+                k.append(s.charAt(i));
             }
             c++;
         }
         else
         {
             c--;
-            if(c>0)k=k+s.charAt(i);
+            if(c>0)k.append(s.charAt(i));
         }
     }
-        return k;
+        return k.toString();
     }
 }
