9bb99892fecae8d4d4a377198b76b41f57aff1c2
sriramsid-dev <mail2sidsriram@gmail.com>
2026-06-29 11:25:12 +0530

    Time: 4 ms (38.39%), Space: 43.9 MB (5.95%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='('||s.charAt(i)=='['||s.charAt(i)=='{')
+            {
+                stack.push(s.charAt(i));
+            }
+            else
+            {
+                if(stack.isEmpty())
+                {
+                    return false;
+                }
+                if(s.charAt(i)==')'&&stack.peek()=='(')
+                {
+                    stack.pop();
+                }
+                else if(s.charAt(i)==']'&&stack.peek()=='[')
+                {
+                    stack.pop();
+                }
+                else if(s.charAt(i)=='}'&&stack.peek()=='{')
+                {
+                    stack.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        if(stack.isEmpty())
+        {
+            return true;
+        }
+        return false;
+    }
+}
