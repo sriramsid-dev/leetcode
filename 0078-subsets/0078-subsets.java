b1a8bfb61de57d05fbf1bf02051465027344438f
sriramsid-dev <mail2sidsriram@gmail.com>
2026-07-13 14:53:43 +0530

    Time: 1 ms (87.15%), Space: 44.1 MB (75.7%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,21 @@
+class Solution {
+    List<List<Integer>> l;
+    public List<List<Integer>> subsets(int[] nums) {
+        l=new ArrayList<>();
+        List<Integer> l1=new ArrayList<>();
+        solve(nums,0,l1);
+        return l;
+    }
+    public void solve(int[] nums,int i,List<Integer> l1)
+    {
+        if(i>=nums.length)
+        {
+            l.add(new ArrayList<>(l1));
+            return;
+        }
+        l1.add(nums[i]);
+        solve(nums,i+1,l1);
+        l1.remove(l1.get(l1.size()-1));
+        solve(nums,i+1,l1);
+    }
+}
